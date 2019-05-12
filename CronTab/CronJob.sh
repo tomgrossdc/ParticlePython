174adf4e06951805d6febb4f8bc71e02effd0e99
tomgrossdc <tommyerols@gmail.com>
2019-05-12 12:54:07 -0400

    deletes

--- CronTab/CronJob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# CronJob.sh
-export WGETWORKDIR=/media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB
-echo " CRONJob.sh with wget_regular_test.py WGETWORKDIR = ", $WGETWORKDIR
-cd /media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB
-python3 wget_regular_tomMy.py >> /media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB/junk.log
-date
-echo "THE END"
-
