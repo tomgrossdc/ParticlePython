e03653469c91ebaa27292037c78d17035a59970c
Tom Gross <26555901+tomgrossdc@users.noreply.github.com>
2019-05-12 12:56:23 -0400

    Update CronJob.sh

--- CronJob/CronJob.sh
+++ CronJob/CronJob.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # CronJob.sh
+# add to your crontab -e 
+# 13 20 * * * /media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB/CronJob.sh &> /media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB/cronjoblog.log
+
 export WGETWORKDIR=/media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB
 echo " CRONJob.sh with wget_regular_test.py WGETWORKDIR = ", $WGETWORKDIR
 cd /media/tom/MyBookAllLinux/NOSnetcdf/CRONTAB
